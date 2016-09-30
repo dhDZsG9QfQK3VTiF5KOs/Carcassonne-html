05ceb320581e6f1806f9f318ec937b159fc58c6e
Solomon Ucko <solly.ucko@gmail.com>
2016-09-30 08:24:42 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+row = document.createElement("tr");
+
+cell = document.createElement("td");
+cell.innerText = "test";
+row.appendChild(cell);
+
+cell = document.createElement("td");
+cell.innerText = "test";
+row.appendChild(cell);
+
+document.getElementById("tiles").appendChild(row);
+
+row = document.createElement("tr");
+
+cell = document.createElement("td");
+cell.innerText = "test";
+row.appendChild(cell);
+
+cell = document.createElement("td");
+cell.innerText = "test";
+row.appendChild(cell);
+
+document.getElementById("tiles").appendChild(row);
